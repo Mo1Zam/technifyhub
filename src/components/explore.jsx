2cabf939daaacc8e0b2e9f492779027cba775e8b
Mohd Azam <sayedsha0021@gmail.com>
2024-08-02 17:41:12 +0530

    minor change

--- src/components/explore.jsx
+++ src/components/explore.jsx
@@ -4,7 +4,7 @@ export default function Explore() {
             <div className="bg-slate-400 p-5 gap-10 flex flex-col ">
                 <h1 className="text-7xl uppercase text-center font-bold">explore top courses</h1>
                 <div className="flex flex-col gap-5 justify-center items-center  sm:flex-row">
-                    <Card heading="windows" font="font-light" image="src\assets\logo.svg"/>
+                    <Card heading="windows" font="font-light" />
                     <Card heading="cyberark" font="font-bold"/>
                     <Card heading="linux" font="font-light"/>   
                 </div>
@@ -17,7 +17,6 @@ function Card({image, heading , para, font}) {
     return(
         <div className="bg-violet-500 w-60 h-96 rounded-2xl text-center mt-3">
             <h1 className={`text-4xl my-5 uppercase text-white ${font}`}>{heading}</h1>
-            <div className={`url('${image}')`}></div>
         </div>
     )
 }
