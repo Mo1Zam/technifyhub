3a43308e331d635f5d7d577a55b791182f8968b6
Mohd Azam <sayedsha0021@gmail.com>
2024-08-29 23:47:12 +0530

    button link

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,7 +1,7 @@
 function PrimaryButton({name="primary",bgcolor="bg-blue-800",textColor="text-white", width="sm:w-32", borderColor="border-blue-700"}) {
     return (
         <>
-            <button className={`uppercase border-solid border-2 ${borderColor} ${bgcolor} rounded font-semibold w-full  ${textColor} p-1 ${width}`}>{name}</button>
+            <a href="https://6oqd0yd1wcp.typeform.com/to/WUVSAkiV" className={`uppercase border-solid border-2 ${borderColor} ${bgcolor} rounded font-semibold w-full  ${textColor} p-1 ${width}`}>{name}</a>
         </>
     )
 }
--- src/components/buttons/SecondryButton.jsx
+++ src/components/buttons/SecondryButton.jsx
@@ -1,7 +1,7 @@
 export default function SecondryButton({name="secondry"}){
     return(
         <>
-            <button className="uppercase border-solid border-2 border-blue-300 rounded w-full p-1 sm:w-32">{name}</button>
+            <a href="https://6oqd0yd1wcp.typeform.com/to/WUVSAkiV" className="uppercase border-solid border-2 border-blue-300 rounded w-full p-1 sm:w-32">{name}</a>
         </>
     )
 }
